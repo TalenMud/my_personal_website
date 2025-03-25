7f1042993d5ee3ecf934a2f73bcfdebe0baf92a8
TalenMud <talenmudaly@gmail.com>
2025-03-25 13:11:17 +0000

    added button styling

--- colorScript.js
+++ colorScript.js
@@ -1,6 +1,7 @@
 function Changecolor () {
-    const colors = ["white", "blue", "red", "green"];
+    const colors = ["white", "blue", "red", "green", "#01ffff"];
     const RandomColor = colors[Math.floor(Math.random() * colors.length)];
+
 
     document.body.style.backgroundColor = RandomColor;
 
